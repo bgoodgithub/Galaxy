db90edc82b72dd9995b8761249ea41cbaa6efa4b
bgoodgithub <brock@islandmojo.com>
2021-05-20 12:54:35 -0400

    cleaned up code, eliminated unused functions, created new files and relocated transforms and user inputs

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from kivy.uix.widget import Widget
 
 
 class MainWidget(Widget):
+    from transforms import transform, transform_2d, transform_perspective
+    from user_actions import keyboard_closed, on_keyboard_up, on_keyboard_down, on_touch_down, on_touch_up
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
@@ -43,36 +45,11 @@ class MainWidget(Widget):
 
         Clock.schedule_interval(self.update, 1.0 / 60.0)
 
-    def keyboard_closed(self):
-        self._keyboard.unbind(on_key_down=self.on_keyboard_down)
-        self._keyboard.unbind(on_key_up=self.on_keyboard_up)
-        self._keyboard = None
-
     def is_dektop(self):
         if platform in ("linux", "win", "macosx"):
             return True
         else:
             return False
-
-    def on_parent(self, widget, parent):
-        # print("ON PARENT W: " + str(self.width) + " H: " + str(self.height))
-        pass
-
-    def on_size(self, *args):
-        # print("ON SIZE W: " + str(self.width) + " H: " + str(self.height))
-        # self.perspective_point_x = self.width/2
-        # self.perspective_point_y = self.height * 0.75
-        # self.update_vertical_lines()
-        # self.update_horizontal_lines()
-        pass
-
-    def on_perspective_point_x(self, widget, value):
-        # print("PX: " + str(value))
-        pass
-
-    def on_perspective_point_y(self, widget, value):
-        # print("PY: " + str(value))
-        pass
 
     def init_vertical_lines(self):
         with self.canvas:
@@ -113,51 +90,6 @@ class MainWidget(Widget):
             x1, y1 = self.transform(xmin, line_y)
             x2, y2 = self.transform(xmax, line_y)
             self.horizontal_lines[i].points = [x1, y1, x2, y2]
-
-    def transform(self, x, y):
-        # return self.transform_2d(x, y)
-        return self.transform_perspective(x, y)
-
-    def transform_2d(self, x, y):
-        return int(x), int(y)
-
-    def transform_perspective(self, x, y):
-        lin_y = y * self.perspective_point_y / self.height
-        if lin_y > self.perspective_point_y:
-            lin_y = self.perspective_point_y
-
-        diff_x = x - self.perspective_point_x
-        diff_y = self.perspective_point_y - lin_y
-        factor_y = diff_y / self.perspective_point_y
-        factor_y = pow(factor_y, 2)  # increasing the power gives the illusion of moving faster
-
-        tr_x = self.perspective_point_x + diff_x * factor_y
-        tr_y = (1 - factor_y) * self.perspective_point_y
-
-        return int(tr_x), int(tr_y)
-
-    def on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        if keycode[1] == "left":
-            self.current_speed_x = self.speed_x
-        elif keycode[1] == "right":
-            self.current_speed_x = -self.speed_x
-        return True
-
-    def on_keyboard_up(self, keyboard, keycode):
-        self.current_speed_x = 0
-        return True
-
-    def on_touch_down(self, touch):
-        if touch.x < self.width / 2:
-            # print("<-")
-            self.current_speed_x = self.speed_x
-        if touch.x > self.width / 2:
-            # print("->")
-            self.current_speed_x = -self.speed_x
-
-    def on_touch_up(self, touch):
-        # print("UP")
-        self.current_speed_x = 0
 
     def update(self, dt):
         # print(str(dt*60))
